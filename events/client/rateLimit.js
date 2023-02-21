a6bb21f54c386b0002374be7c6af270dfe4dc8f7
alwaysluxury <102433141+alwaysluxury@users.noreply.github.com>
2023-02-21 03:40:30 +0100

    Create rateLimit.js

---
+++ events/client/rateLimit.js
@@ -0,0 +1,16 @@
+/* 
+Made by alwaysluxury#7729
+Owner of LuxuryDev
+Website: https://luxurydev.eu
+If you have any problems join https://luxurydev.eu/discord for help!
+*/
+const { white, green, red } = require('chalk');
+module.exports = async (client, info) => {
+    console.log(white('[') + green('INFO') + white('] ') + green(`Bot Is`) + white('RateLimited'));
+}
+/* 
+Made by alwaysluxury#7729
+Owner of LuxuryDev
+Website: https://luxurydev.eu
+If you have any problems join https://luxurydev.eu/discord for help!
+*/
