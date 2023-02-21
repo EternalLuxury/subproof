8ad4fb529309e5719b9da535e54b3f013f77dba7
alwaysluxury <102433141+alwaysluxury@users.noreply.github.com>
2023-02-21 03:39:42 +0100

    Create disconnect.js

---
+++ events/client/disconnect.js
@@ -0,0 +1,18 @@
+/* 
+Made by alwaysluxury#7729
+Owner of LuxuryDev
+Website: https://luxurydev.eu
+If you have any problems join https://luxurydev.eu/discord for help!
+*/
+
+const { white, green, red } = require('chalk');
+module.exports = async (client) => {
+    console.log(white('[') + green('INFO') + white('] ') + green(`The `) + white('Bot') + green(' Disconnected!'));
+}
+
+/* 
+Made by alwaysluxury#7729
+Owner of LuxuryDev
+Website: https://luxurydev.eu
+If you have any problems join https://luxurydev.eu/discord for help!
+*/
