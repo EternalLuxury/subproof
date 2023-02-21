453aa0c2fd401c7a805ff023f29aa1bcf0adab57
alwaysluxury <102433141+alwaysluxury@users.noreply.github.com>
2023-02-21 03:41:49 +0100

    Create reconnecting.js

---
+++ events/client/reconnecting.js
@@ -0,0 +1,17 @@
+/* 
+Made by alwaysluxury#7729
+Owner of LuxuryDev
+Website: https://luxurydev.eu
+If you have any problems join https://luxurydev.eu/discord for help!
+*/
+
+const { white, green, red } = require('chalk');
+module.exports = async (client) => {
+    console.log(white('[') + green('INFO') + white('] ') + green(`Bot Is `) + white('Reconnecting'));
+}
+/* 
+Made by alwaysluxury#7729
+Owner of LuxuryDev
+Website: https://luxurydev.eu
+If you have any problems join https://luxurydev.eu/discord for help!
+*/
