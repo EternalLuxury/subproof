6a2bbe2beee031a399742a0ff292aee4c80f4a2e
alwaysluxury <102433141+alwaysluxury@users.noreply.github.com>
2023-02-21 03:40:06 +0100

    Create error.js

---
+++ events/client/error.js
@@ -0,0 +1,18 @@
+/* 
+Made by alwaysluxury#7729
+Owner of LuxuryDev
+Website: https://luxurydev.eu
+If you have any problems join https://luxurydev.eu/discord for help!
+*/
+const { white, green, red } = require('chalk');
+module.exports = async (client, error) => {
+    
+    console.log(white('[') + red('INFO') + white('] ') + red(`An Error `) + white('Occured') + red(` ${error}`));
+}
+
+/* 
+Made by alwaysluxury#7729
+Owner of LuxuryDev
+Website: https://luxurydev.eu
+If you have any problems join https://luxurydev.eu/discord for help!
+*/
